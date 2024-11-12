06827b9d32944b4b4f8d9e52d4606a646345cfbb
Flugel <151924761+Eu4ngelion@users.noreply.github.com>
2024-11-12 13:33:23 +0800

    pengaturan body

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require "koneksi.php";
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-if(!isset($_SESSION["login"])){
+if (!isset($_SESSION["login"])) {
     $_SESSION["login"] = false;
 }
 
@@ -17,16 +17,36 @@ $sql = "SELECT nama_admin FROM admin";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Koruko</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .main-index {
+            height: auto;
+            min-height: 100vh;
+            padding-top: 80px;
+        }
+
+    </style>
 </head>
-<body>
-    <?php
-    include "navbar.php";
-    ?>
-    <h1>Beranda</h1>
+
+
+<body class="body-index">
+    <header><?php include "navbar.php";?></header>
+
+    <main class="main-index">
+        <h1>Beranda</h1>
+    </main>
+
+    <footer><?php include "footer.php"; ?></footer>
 </body>
+
+
 </html>

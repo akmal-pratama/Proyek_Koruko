5452fb526136b80a8b1362fdaf3ba985508e12e5
Flugel <151924761+Eu4ngelion@users.noreply.github.com>
2024-11-12 12:45:13 +0800

    struktur tentang

--- tentang.php
+++ tentang.php
@@ -1,15 +1,52 @@
+<?php
+require "koneksi.php";
+
+// Memulai Sesion
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION["login"])) {
+    $_SESSION["login"] = false;
+}
+
+// Mengambil data admin
+$sql = "SELECT nama_admin FROM admin";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tentang Kami</title>
+    <title>Tentang</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .main-index {
+            height: auto;
+            min-height: 100vh;
+            padding-top: 80px;
+        }
+
+    </style>
 </head>
-<body>
-    <?php
-    include "navbar.php";
-    ?>
-    <h1>Tentang Kami</h1>
+
+
+<body class="body-index">
+    <header><?php include "navbar.php";?></header>
+
+    <main class="main-index">
+        <h1>Tentang</h1>
+    </main>
+
+    <footer><?php include "footer.php"; ?></footer>
 </body>
+
+
 </html>
